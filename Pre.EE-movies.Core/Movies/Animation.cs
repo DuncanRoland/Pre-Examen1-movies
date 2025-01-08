6e0bc33c68b5c869fc5871e5a6a9f8d8f1f7450e
Duncan <r0897472@student.vives.be>
2025-01-08 15:56:02 +0100

    Add the Animation class

---
+++ Pre.EE-movies.Core/Movies/Animation.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using Pre.EE_movies.Core.Enums;
+
+namespace Pre.EE_movies.Core.Movies;
+
+public class Animation : Movie
+{
+    public string AnimationStudio { get; set; }
+    public AnimationType AnimationType { get; set; }
+
+    public Animation(string title, string genre, DateTime releaseDate, int minAge, Director directorMovie,
+        string animationStudio, AnimationType animationType) : base(title, genre, releaseDate, minAge, directorMovie)
+    {
+        AnimationStudio = animationStudio;
+        AnimationType = animationType;
+    }
+}
