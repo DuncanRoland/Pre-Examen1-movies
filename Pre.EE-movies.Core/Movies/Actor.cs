092fdeb90ab7fb8d0bf8f02eb1ba7e88e7d079f5
Duncan <r0897472@student.vives.be>
2025-01-08 14:43:19 +0100

    Add actor class

--- Pre.EE-movies.Core/Movies/Actor.cs
+++ Pre.EE-movies.Core/Movies/Actor.cs
@@ -1,6 +1,19 @@
-﻿namespace Pre.EE_movies.Core.Movies;
+﻿using Pre.EE_movies.DLL.Interfaces;
 
-public class Actor
+namespace Pre.EE_movies.Core.Movies;
+
+public class Actor : Person
 {
-    
+    public int AwardsCount { get; }
+
+    public Actor(string name, DateTime birthdate) : base(name, birthdate)
+    {
+    }
+
+    public int AddAward(int award ) => AwardsCount + award;
+
+    public override string WatchMovie(IMovie movie)
+    {
+        throw new NotImplementedException();
+    }
 }
