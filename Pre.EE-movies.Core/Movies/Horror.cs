99145360bbe8c6a09c87a4a89fb84fb136c6e627
Duncan <r0897472@student.vives.be>
2025-01-08 15:59:24 +0100

    Add the Horror class

---
+++ Pre.EE-movies.Core/Movies/Horror.cs
@@ -0,0 +1,11 @@
+﻿namespace Pre.EE_movies.Core.Movies;
+
+public class Horror : Movie
+{
+    public int ScareFactor { get; set; }
+    
+    public Horror(string title, string genre, DateTime releaseDate, int minAge, Director directorMovie, int scareFactor) : base(title, genre, releaseDate, minAge, directorMovie)
+    {
+        ScareFactor = scareFactor;
+    }
+}
