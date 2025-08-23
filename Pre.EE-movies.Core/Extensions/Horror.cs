ba5f8fba9b21c83067f2b60b596a97079577be73
Duncan Roland <duncan.roland@student.howest.be>
2025-08-23 21:47:59 +0200

    Add the horror extension method

---
+++ Pre.EE-movies.Core/Extensions/Horror.cs
@@ -0,0 +1,19 @@
+﻿using Pre.EE_movies.Core.Movies;
+
+namespace Pre.EE_movies.Core.Extensions;
+
+public class Horror : Movie
+{
+    public int ScareFactor { get; set; }
+    
+    public Horror(string title, string genre, DateTime releaseDate, int minAge, Director directorMovie, int scareFactor) : base(title, genre, releaseDate, minAge, directorMovie)
+    {
+        this.ScareFactor = scareFactor;
+    }
+
+    public void SetScareFactor(int scareFactor)
+    {
+        ScareFactor = scareFactor;
+    }
+    
+}
