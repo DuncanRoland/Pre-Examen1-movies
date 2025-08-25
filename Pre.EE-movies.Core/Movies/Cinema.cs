9a0757a4b8c67169e4d384b72edd7dd0e7c7976b
Duncan Roland <duncan.roland@student.howest.be>
2025-08-25 20:56:45 +0200

    Update the Cinema class

--- Pre.EE-movies.Core/Movies/Cinema.cs
+++ Pre.EE-movies.Core/Movies/Cinema.cs
@@ -6,24 +6,29 @@ namespace Pre.EE_movies.Core.Movies;
 
 public class Cinema : ICinema
 {
-    public string CinemaName { get; set; }
+    private string CinemaName { get; set; }
+    private MovieService MovieService { get; }
+    private Random _random;
 
-    private MovieService _movieService { get; }
-
-    private Random random;
-
-    public Cinema(string cinemaName)
+    public Cinema(string cinemaName, MovieService movieService)
     {
         CinemaName = cinemaName;
+        MovieService = movieService ?? throw new ArgumentNullException(nameof(movieService));
+        _random = new Random();
     }
+
+    // Explicit setter as required by UML
+    public void SetCinemaName(string name)
+    {
+        CinemaName = name;
+    }
+    
 
     public List<IMovie> LoadMoviesAsync(string path, string format)
     {
         throw new NotImplementedException();
-        //return
     }
-    
-    
+
     public void AddMovie(IMovie movie)
     {
         throw new NotImplementedException();
